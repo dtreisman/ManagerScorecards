44aac87bf52ba26351a3f885c2c90fca2433e72a
dtreisman <57765116+dtreisman@users.noreply.github.com>
2022-10-08 20:06:32 -0500

    add main script

---
+++ 02-get_daily_results.R
@@ -0,0 +1,252 @@
+library(gt)
+library(baseballr)
+library(tidyverse)
+library(lubridate)
+library(slider)
+library(randomForest)
+library(googledrive)
+
+source("0x-helper_functions.R")
+
+
+# Get pre-scraped active rosters with positions
+rosters <- read_csv(url("https://github.com/dtreisman/DailyMLBRosters/raw/main/data/DailyMLBRosters.csv"))
+pitchers <- rosters %>% 
+  filter(pos %in% c("SP", "RP")) %>%
+  mutate(team_abbr = str_to_upper(team_abbr)) %>%
+  select(name, pos, jersey, bats, throws, team_abbr, date, player_id) %>%
+  mutate(date = lubridate::as_date(date),
+         jersey = as.numeric(jersey))
+
+
+# get MLB.com active rosters
+al_teams <- tibble(baseballr::mlb_teams(season = 2022, league_ids = c(103)))
+nl_teams <- tibble(baseballr::mlb_teams(season = 2022, league_ids = c(104)))
+al_teams[which(al_teams$team_abbreviation == "CWS"), "team_abbreviation"] <- "CHW"
+
+mlb_rosters <- tibble()
+for (day in seq.Date(Sys.Date()-1, Sys.Date()-1, by = "1 day")) { # seq.Date(Sys.Date()-9, Sys.Date()-9, by = "1 day")) {
+  print(as_date(day))
+  for (i in 1:length(c(al_teams$team_abbreviation, nl_teams$team_abbreviation))) {
+    abbrs <- c(al_teams$team_abbreviation, nl_teams$team_abbreviation)
+    ids <- c(al_teams$team_id, nl_teams$team_id)
+    #print(abbrs[i])
+    temp <- mlb_rosters(team_id = c(ids[i]), season = 2022, roster_type = 'active', date = as_date(day))
+    
+    temp <- temp %>% 
+      select(person_id, person_full_name, jersey_number) %>%
+      mutate(jersey_number = as.numeric(jersey_number))
+    temp$date <- as_date(day)
+    temp$team <- abbrs[i]
+    mlb_rosters <- bind_rows(mlb_rosters, temp)
+  }
+  
+}
+
+all_mlb_rosters <- readRDS("data/MLBRosters.Rds")
+all_mlb_rosters <- bind_rows(all_mlb_rosters, mlb_rosters)
+saveRDS(object = all_mlb_rosters, file = "data/MLBRosters.Rds")
+
+new_games <- scrape_statcast_savant_pitcher_all(start_date = Sys.Date()-1, end_date = Sys.Date()-1)
+
+if (nrow(new_games > 0)) {
+  
+  df_current <- readRDS(file = "data/df_current.Rds")
+  df_current <- bind_rows(df_current, new_games)
+  df_current <- saveRDS(object = df_current, file = "data/df_current.Rds")
+  googledrive::drive_download("https://drive.google.com/file/d/1MisIarJqBGJiB_0qPhE8RTihaFhB0_JT/view?usp=sharing",
+                              path = here::here(), overwrite = T)
+  
+  new_games <- new_games %>%#scrape_statcast_savant_pitcher_all(start_date = Sys.Date()-9, end_date = Sys.Date()-9) %>%
+    filter(events != "", !is.na(events)) %>%
+    mutate(inning = ifelse(inning > 9, 10, inning),
+           inning = as.factor(inning),
+           game_date_string = str_remove_all(game_date, "-"),
+           game_id = paste0(game_date_string, "_", home_team, "_", away_team)) 
+  
+  # new_games <- pullPitcherPBP(2022)
+  # new_games <- new_games %>%
+  #   filter(events != "", !is.na(events)) %>%
+  #   mutate(inning = ifelse(inning > 9, 10, inning),
+  #          inning = as.factor(inning),
+  #          game_date_string = str_remove_all(game_date, "-"),
+  #          game_id = paste0(game_date_string, "_", home_team, "_", away_team)) 
+  
+  
+  
+  
+  
+  # old_games <- df_mod %>%
+  #   filter(game_year == year(Sys.Date()) - 1) %>%
+  #   bind_rows(statcast_22 %>%
+  #               mutate(inning = ifelse(inning > 9, 10, inning),
+  #                      inning = as.factor(inning))) %>%
+  #   prepareNewData()
+  
+  
+  
+  # 
+  # df_new <- df %>%
+  #   bind_rows(df_current) %>%
+  #   bind_rows(new_games)%>%
+  #   prepareNewData()%>%
+  #   mutate(inning = ifelse(inning > 9, 10, inning),
+  #          inning = as.factor(inning))
+  
+  df_new <- df %>%
+    bind_rows(df_current) %>%
+    prepareNewData() %>%
+    mutate(inning = ifelse(inning > 9, 10, inning),
+           inning = as.factor(inning))
+  
+  df_new <- augmentNewData(df_new) %>%
+    padr::fill_by_value(pitcher_woba, mean(old_games$pitcher_woba)) %>%
+    ungroup() %>%
+    mutate(woba_diff = pitcher_woba - batter_woba,
+           inning = as.factor(inning),
+           state = factor(state))
+  
+  
+  df_pred <- df_new %>%
+    filter(game_date == new_games$game_date[1]) 
+  
+  
+  old_games <- readRDS(file = "data/old_games.Rds")
+  old_games <- bind_rows(old_games, df_pred) 
+  saveRDS(object = old_games, file = "data/old_games.Rds")
+  
+  fit_wp <- readRDS("models/win_probability_model.Rds")
+  fit_runs <- readRDS("models/expected_runs_model.Rds")
+  fit_new_pitcher <- readRDS("models/expected_pitching_change_model.Rds")
+  
+  pred_wp <- predict(fit_wp, df_pred, type = "response")
+  pred_runs <- predict(fit_runs, df_pred)
+  pred_new_pitcher <- predict(fit_new_pitcher, df_pred, type = "response")
+  
+  
+  pred_wp_hist <- predict(fit_wp, df_new, type = "response")
+  pred_runs_hist  <- predict(fit_runs, df_new)
+  pred_np_hist  <- predict(fit_new_pitcher, df_new, type = "response")
+  
+  
+  # df_output_history <- df_new %>%
+  # bind_cols(pred_wp_hist) %>%
+  # bind_cols(pred_runs_hist) %>%
+  # bind_cols(pred_np_hist) %>%
+  # rename(pred_wp = ...46,
+  #        pred_runs = ...47,
+  #        pred_new_pitcher = ...48) %>%
+  # # select(game_id, game_date, name, jersey, pitching_team, inning, inning_topbot,
+  # #        pred_wp, pred_runs, is_pitching, state, fld_score, bat_score, n_batters) %>%
+  # mutate(situation_id = paste0(game_id, "_", inning_topbot, "_", inning, "_",
+  #                              state, "_", fld_score, "_", bat_score)) %>%
+  # group_by(situation_id) %>%
+  # mutate(pred_wp = ifelse(pred_wp < 0, 0, pred_wp),
+  #        pred_runs = ifelse(pred_runs < 0, 0, pred_runs),
+  #        max_wp = max(pred_wp),
+  #        current_wp = ifelse(is_pitching == 1, pred_wp, NA),
+  #        min_runs = min(pred_runs),
+  #        current_runs = ifelse(is_pitching == 1, pred_runs, NA)) %>%
+  # fill(current_wp, .direction = "downup") %>%
+  # fill(current_runs, .direction = "downup") %>%
+  # mutate(wp_diff = current_wp - max_wp,
+  #        runs_diff = min_runs - current_runs,
+  #        runs_diff_adj =  ifelse(new_pitcher == 1, 
+  #                                runs_diff * (1 - pred_new_pitcher),
+  #                                runs_diff * -pred_new_pitcher)) %>%
+  # filter(is_pitching == 1, n_batters > 3)
+  
+  
+  df_output <- df_pred %>%
+    bind_cols(pred_wp) %>%
+    bind_cols(pred_runs) %>%
+    bind_cols(pred_new_pitcher) %>%
+    rename(pred_wp = ...46,
+           pred_runs = ...47,
+           pred_new_pitcher = ...48) %>%
+    # select(game_id, game_date, name, jersey, pitching_team, inning, inning_topbot,
+    #        pred_wp, pred_runs, is_pitching, state, fld_score, bat_score, n_batters) %>%
+    mutate(situation_id = paste0(game_id, "_", inning_topbot, "_", inning, "_",
+                                 state, "_", fld_score, "_", bat_score)) %>%
+    group_by(situation_id) %>%
+    mutate(pred_wp = ifelse(pred_wp < 0, 0, pred_wp),
+           pred_runs = ifelse(pred_runs < 0, 0, pred_runs),
+           max_wp = max(pred_wp),
+           current_wp = ifelse(is_pitching == 1, pred_wp, NA),
+           min_runs = min(pred_runs),
+           current_runs = ifelse(is_pitching == 1, pred_runs, NA)) %>%
+    fill(current_wp, .direction = "downup") %>%
+    fill(current_runs, .direction = "downup") %>%
+    mutate(wp_diff = current_wp - max_wp,
+           runs_diff = min_runs - current_runs,
+           runs_diff_adj =  ifelse(new_pitcher == 1, 
+                                   runs_diff * (1 - pred_new_pitcher),
+                                   runs_diff * -pred_new_pitcher)) %>%
+    filter(is_pitching == 1)
+  
+  
+  
+  df_output_history <- readRDS("data/output_history.Rds")
+  df_output_history <- bind_rows(df_output_history, df_output)
+  saveRDS(object = df_output_history, file = "data/output_history.Rds")
+  
+  
+  
+  
+  # by_team <- df_output %>%
+  #   # filter(df_output$game_id %in% )
+  #   filter(is_pitching == 1, n_batters > 3) %>%
+  #   mutate(wp_diff = round(wp_diff, 3),
+  #          runs_diff_adj = round(runs_diff_adj, 3)) %>%
+  #   group_by(pitching_team) %>%
+  #   summarise(wp_diff = sum(wp_diff, na.rm = T),
+  #             runs_diff_adj = sum(runs_diff_adj, na.rm = T)) 
+  
+  dist_history <- df_output_history %>%
+    group_by(game_id, pitching_team) %>%
+    summarise(runs_diff = sum(runs_diff)) 
+  
+  
+  api_key <- Sys.getenv("TWITTERAPIKEY")
+  api_secret <- Sys.getenv("TWITTERAPISECRET")
+  access_token <- Sys.getenv("TWITTERACCESSTOKEN")
+  access_secret <- Sys.getenv("TWITTERACCESSTOKENSECRET")
+  
+  auth <- rtweet::rtweet_bot(api_key = api_key,
+                             api_secret = api_secret, 
+                             access_token = access_token, 
+                             access_secret = access_secret)
+  
+  rtweet::auth_as(auth)
+  
+  
+  yesterday_games <- mlb_schedule(2022) %>% 
+    filter(date == Sys.Date()-1) %>% 
+    select(game_pk) %>%
+    deframe()
+  
+  
+  # mlb_pbp(662955) %>%
+  #   select(contains("OnFirst")) %>%
+  #   names()
+  
+  for (id in yesterday_games) {
+    
+    print(id)
+    tweet <- write_tweet(id, df_output, dist_history)
+    print(tweet)
+    rtweet::post_tweet(tweet)
+    
+    Sys.sleep(10)
+  }
+  
+  
+}
+
+
+
+
+
+
+
+
---
+++ 0x-helper_functions.R
@@ -0,0 +1,283 @@
+prepareTrainingData <- function(df) {
+  df <- df %>%
+    arrange(game_date, home_team, away_team, at_bat_number) %>%
+    mutate(game_date_string = str_remove_all(game_date, "-"),
+           game_id = paste0(game_date_string, "_", home_team, "_", away_team),
+           month = month(game_date),
+           pitching_team = ifelse(inning_topbot == "Top", home_team, away_team),
+           win = ifelse(Winner == pitching_team, 1, 0),
+           on_1b = ifelse(is.na(on_1b), 0, 1),
+           on_2b = ifelse(is.na(on_2b), 0, 1),
+           on_3b = ifelse(is.na(on_3b), 0, 1),
+           state = paste0(outs_when_up, on_1b, on_2b, on_3b),
+           score_diff = fld_score - bat_score,
+           platoon = ifelse(stand == p_throws, 1, 0),
+           runs_scored = post_bat_score - bat_score,
+           one = 1) %>%
+    group_by(pitcher) %>%
+    mutate(pitcher_woba = slider::slide_mean(woba_value,
+                                             before = 100, 
+                                             complete = F, 
+                                             na_rm = T),
+           pitcher_n_app = unlist(slider::slide_index(.x = one,
+                                               .i = game_date,
+                                               .f = ~sum(., na.rm = T),
+                                             .before = 100, 
+                                             .complete = F))) %>%
+    group_by(batter) %>%
+    mutate(batter_woba = slider::slide_mean(woba_value,
+                                            before = 100, 
+                                            complete = F, 
+                                            na_rm = T),
+           batter_n_app = unlist(slider::slide_index(.x = one, 
+                                            .i = game_date,
+                                            .f = ~sum(., na.rm = T), 
+                                            .before = 100, 
+                                            .complete = F))) %>%
+    group_by(pitcher, game_pk) %>%
+    #arrange(game_date, inning, desc(inning_topbot)) %>%
+    padr::fill_by_value(woba_value, 0) %>%
+    mutate(one = 1,
+           n_batters = cumsum(one),
+           pitcher_game_woba = cummean(woba_value)*n_batters) %>%
+    ungroup() %>%
+    mutate(avg_woba = slider::slide_mean(x = woba_value, 
+                                         before = 5000, 
+                                         complete = F,
+                                         na_rm = T),
+           pitcher_woba = ifelse(pitcher_n_app <=15, avg_woba, pitcher_woba),
+           batter_woba = ifelse(batter_n_app <=15, avg_woba, batter_woba)) %>%
+    group_by(game_id, pitching_team) %>%
+    mutate(new_pitcher = ifelse(pitcher != lag(pitcher), 1, 0))%>%
+    ungroup() %>%
+    padr::fill_by_value(new_pitcher, value = 0)
+  
+  return(df)
+}
+
+prepareNewData <- function(df) {
+  df <- df %>%
+    select(game_date, game_year, game_pk, player_name, pitcher, batter, events, description, 
+           stand, p_throws, home_team, away_team, home_score, away_score, 
+           on_1b, on_2b, on_3b, outs_when_up, inning, inning_topbot, woba_value,
+           bat_score, fld_score, post_bat_score, at_bat_number) %>%
+    arrange(game_date, home_team, away_team, at_bat_number) %>%
+    mutate(game_date_string = str_remove_all(game_date, "-"),
+           game_id = paste0(game_date_string, "_", home_team, "_", away_team),
+           month = month(game_date),
+           pitching_team = ifelse(inning_topbot == "Top", home_team, away_team),
+           on_1b = ifelse(is.na(on_1b), 0, 1),
+           on_2b = ifelse(is.na(on_2b), 0, 1),
+           on_3b = ifelse(is.na(on_3b), 0, 1),
+           state = paste0(outs_when_up, on_1b, on_2b, on_3b),
+           score_diff = fld_score - bat_score,
+           platoon = ifelse(stand == p_throws, 1, 0),
+           runs_scored = post_bat_score - bat_score,
+           one = 1) %>%
+    group_by(pitcher) %>%
+    mutate(pitcher_woba = slider::slide_mean(woba_value,
+                                             before = 100, 
+                                             complete = F, 
+                                             na_rm = T),
+           pitcher_n_app = unlist(slider::slide_index(.x = one,
+                                                      .i = game_date,
+                                                      .f = ~sum(., na.rm = T),
+                                                      .before = 100, 
+                                                      .complete = F))) %>%
+    group_by(batter) %>%
+    mutate(batter_woba = slider::slide_mean(woba_value,
+                                            before = 100, 
+                                            complete = F, 
+                                            na_rm = T),
+           batter_n_app = unlist(slider::slide_index(.x = one, 
+                                                     .i = game_date,
+                                                     .f = ~sum(., na.rm = T), 
+                                                     .before = 100, 
+                                                     .complete = F))) %>%
+    group_by(pitcher, game_pk) %>%
+    #arrange(game_date, inning, desc(inning_topbot)) %>%
+    padr::fill_by_value(woba_value, 0) %>%
+    mutate(one = 1,
+           n_batters = cumsum(one),
+           pitcher_game_woba = cummean(woba_value)*n_batters) %>%
+    ungroup() %>%
+    mutate(avg_woba = slider::slide_mean(x = woba_value, 
+                                         before = 5000, 
+                                         complete = F,
+                                         na_rm = T))%>%
+    group_by(game_id, pitching_team) %>%
+    mutate(new_pitcher = ifelse(pitcher != lag(pitcher), 1, 0)) %>%
+    ungroup() %>%
+    padr::fill_by_value(new_pitcher, value = 0) %>%
+    padr::fill_by_value(batter_n_app, value = 0) 
+  
+  return(df)
+}
+
+df_new %>%
+  count(game_date)
+augmentNewData <- function(df_new){
+  df_new <- df_new %>%
+    select(-c(events, description, home_team, away_team, home_score, away_score,
+              on_1b, on_2b, on_3b, outs_when_up, one)) %>%
+    left_join(pitchers, 
+              by = c("pitching_team" = "team_abbr", "game_date" = "date")) %>%
+    left_join(all_mlb_rosters, 
+              by = c("pitching_team" = "team", "jersey" = "jersey_number", "game_date" = "date")) %>%
+    mutate(is_pitching = ifelse(person_id == pitcher, 1, 0),
+           pitcher_woba = ifelse(is_pitching == 0, NA, pitcher_woba)) %>%
+    group_by(person_id) %>%
+    #mutate(pitcher_woba = ifelse(is.na(pitcher_woba), lag(pitcher_woba), pitcher_woba)) %>%
+    fill(pitcher_woba, .direction = "downup") %>%
+    fill(pitcher_n_app, .direction = "down") %>%
+    group_by(game_pk, person_id) %>%
+    mutate(n_batters_tot = cumsum(is_pitching)) %>%
+    group_by(game_id, person_id, is_pitching) %>%
+    mutate(pitcher_game_woba = cummean(woba_value*n_batters_tot),
+           platoon = ifelse(stand == throws, 1, 0)) %>% 
+    group_by(pitcher, game_id) %>%
+    #arrange(game_date, inning, desc(inning_topbot)) %>%
+    ungroup() %>%
+    mutate(pitcher_woba = ifelse(pitcher_n_app <= 15, avg_woba, pitcher_woba),
+           batter_woba = ifelse(batter_n_app <= 15, avg_woba, batter_woba)) %>%
+    filter(game_year == 2022, !is.na(is_pitching)) %>%
+    mutate(available = ifelse((pos == "RP" & n_batters_tot == 0) | (is_pitching == 1), 1, 0)) %>%
+    # mutate(available = ifelse((pos == "RP" & n_batters_tot == 0) | (pos == "SP" & n_batters_tot > 0), 1, 0)) %>%
+    filter(available == 1)
+  
+  df_days_rest_new <- df_new %>%
+    filter(is_pitching == 1) %>%
+    select(game_pk, game_date, person_id, name, is_pitching) %>%
+    group_by(person_id, name, game_date) %>%
+    summarise(n_batters_tot = sum(is_pitching)) %>%
+    #group_by(person_id, name, game_date) %>%
+    mutate(days_rest = as.numeric(game_date - lag(game_date)),
+           days_rest = ifelse(days_rest > 5, 5, days_rest),
+           one = 1,
+           n_games_last_5 = unlist(slide_index(.x = one, .i = game_date, .f = sum, .before = 5))-1) %>%
+    ungroup() %>%
+    padr::fill_by_value(days_rest, value = 5) %>%
+    select(-n_batters_tot)
+  
+  
+  df_new <- df_new %>%
+    left_join(df_days_rest_new %>% 
+                select(-c(name, one)), by = c("person_id", "game_date")) %>%
+    fill(days_rest, .direction = "down") %>%
+    fill(n_games_last_5, .direction = "down") %>%
+    padr::fill_by_value(days_rest, value = 5) %>%
+    padr::fill_by_value(n_games_last_5, value = 0) %>%
+    padr::fill_by_value(pitcher_woba, mean(old_games$pitcher_woba)) %>%
+    ungroup() %>%
+    mutate(woba_diff = pitcher_woba - batter_woba)
+  
+  return(df_new)
+}
+
+
+write_tweet <- function(id, df_output, dist_history){
+  
+  team_info <- mlb_teams(season = 2022, sport_ids = 1) %>%
+    select(team_full_name, team_abbreviation)
+  
+  yesterday_scores <- mlb_schedule(2022) %>% 
+    filter(date == Sys.Date() - 1)  %>% 
+    select(game_pk, 
+           date, 
+           teams_away_team_name, teams_away_score, 
+           teams_home_team_name, teams_home_score) %>%
+    left_join(team_info %>% rename(away_abbr = team_abbreviation), 
+              by = c("teams_away_team_name" = "team_full_name"))  %>% 
+    left_join(team_info %>% rename(home_abbr = team_abbreviation), 
+              by = c("teams_home_team_name" = "team_full_name")) %>%
+    mutate(game_date_string = str_remove_all(date, "-"),
+           game_id = paste0(game_date_string, "_", home_abbr, "_", away_abbr)) %>%
+    select(game_date_string, game_pk, home_abbr, teams_home_score, away_abbr, teams_away_score) 
+  
+  output <- df_output %>%
+    separate(game_id, into = c("game_date_string", "home", "away")) %>%
+    left_join(yesterday_scores %>% select(-game_pk), by = c("game_date_string", "home" = "home_abbr", "away" = "away_abbr")) %>%
+    # filter(df_output$game_id %in% )
+    filter(is_pitching == 1, n_batters > 3) %>%
+    mutate(runs_diff_adj = round(runs_diff_adj, 2)) %>%
+    mutate(game_id = paste0(game_date_string, "_", home, "_", away)) %>%
+    # View()
+    group_by(game_id, game_pk, pitching_team) %>%
+    summarise(runs_diff_adj = sum(runs_diff_adj, na.rm = T)) %>%
+    separate(game_id, into = c("game_date_string", "home", "away")) %>%
+    #select(-wp_diff) %>%
+    filter(game_pk == id) %>%
+    pivot_wider(id_cols = game_date_string:game_pk, names_from = pitching_team, values_from = runs_diff_adj)%>%
+    left_join(yesterday_scores %>%
+                select(-game_date_string), by = c("game_pk", "home" = "home_abbr", "away" = "away_abbr")) 
+  
+  game_date <-  paste0(month(as_date(output$game_date_string), label = T), 
+                       " ", 
+                       day(as_date(output$game_date_string)),
+                       ", ",
+                       year(as_date(output$game_date_string)))
+  
+  home_team <- output$home
+  away_team <- output$away
+  away_score <- output$teams_home_score
+  home_score <- output$teams_away_score
+  
+  
+  if (length(names(output)) != 8 | nrow(output) == 0) {
+    
+    output <- yesterday_scores %>%
+      filter(game_pk == id)
+    
+    home_team <- output$home_abbr
+    away_team <- output$away_abbr
+    away_score <- output$teams_home_score
+    home_score <- output$teams_away_score
+    
+    game_date <-  paste0(month(as_date(output$game_date_string), label = T), 
+                         " ", 
+                         day(as_date(output$game_date_string)),
+                         ", ",
+                         year(as_date(output$game_date_string)))
+    
+    
+    tweet = glue::glue(
+      
+      "{game_date}
+    {away_team} @ {home_team}
+    
+    Final Score: {away_score} - {home_score}
+  
+    Error in data source. Scorecard not available.
+    
+    "
+    ) %>% substr(1, 278)
+  } else {
+    
+    
+    
+    home_run_diff <- deframe(output[,5])
+    away_run_diff <- deframe(output[,6])
+    
+    away_score <- output$teams_home_score
+    home_score <- output$teams_away_score
+    
+    h_percentile <- round(mean(deframe(output[,5]) > dist_history$runs_diff) * 100)
+    a_percentile <- round(mean(deframe(output[,6]) > dist_history$runs_diff) * 100)
+    
+    tweet <- glue::glue(
+      
+      "{game_date}
+    {away_team} @ {home_team}
+    
+    Final Score: {away_score} - {home_score}
+    
+    
+    {away_team} xRuns Lost: {away_run_diff} (Percentile: {a_percentile}%)
+    {home_team} xRuns Lost: {home_run_diff} (Percentile: {h_percentile}%)
+    
+    "
+    ) %>% substr(1, 278)
+  }
+  
+  return(tweet)
+}
