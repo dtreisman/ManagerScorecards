05dcf7c9cad6156af8773e8c9d99eaae0dc8ca5c
dtreisman <57765116+dtreisman@users.noreply.github.com>
2023-03-26 20:57:47 -0500

    Update 0x-helper_functions.R

--- 0x-helper_functions.R
+++ 0x-helper_functions.R
@@ -140,9 +140,9 @@ augmentNewData <- function(df_new){
     mutate(pitcher_woba = ifelse(pitcher_n_app <= 15, avg_woba, pitcher_woba),
            batter_woba = ifelse(batter_n_app <= 15, avg_woba, batter_woba)) %>%
     filter(game_year == 2023, !is.na(is_pitching)) %>%
-    mutate(available = ifelse((pos == "RP" & n_batters_tot == 0) | (is_pitching == 1), 1, 0))# %>%
+    mutate(available = ifelse((pos == "RP" & n_batters_tot == 0) | (is_pitching == 1), 1, 0)) %>%
     # mutate(available = ifelse((pos == "RP" & n_batters_tot == 0) | (pos == "SP" & n_batters_tot > 0), 1, 0)) %>%
-    #filter(available == 1)
+    filter(available == 1)
   
   print(paste("filtered augmented_rows:", as.character(nrow(df_new))))
   
